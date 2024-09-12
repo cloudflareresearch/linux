7509a5b13259605e05a283178098e75235049075
Jan Winkelmann (keks) <jan@cryspen.com>
2024-09-12 16:49:57 +0200

    also build go signing tool and install into vm

--- WORK/launch.sh
+++ WORK/launch.sh
@@ -1,4 +1,7 @@
+#!/usr/bin/bash
+
 set -e
+set -x
 
 name="$1"
 
@@ -25,12 +28,21 @@ go build -o ecdsa ecdsa.go
 go test -o ecdsa.test -c ecdsa*.go
 popd
 
+pushd "$kerneldir/zeta/signing_tool/"
+go build -o signing_tool
+go test -o signing_tool.test -c
+popd
+
+mkdir -p "$workdir/virtme-home/bin"
+
 # prepare home bin dir
 pushd "$workdir/virtme-home/bin/"
-rm -- *
+rm -- * || true # it's okay if there isn't anything
 ln -s "$kerneldir/zeta/rsa_bench/rsa"
 ln -s "$kerneldir/zeta/ecdsa_bench/ecdsa"
 ln -s "$kerneldir/zeta/ecdsa_bench/ecdsa.test"
+ln -s "$kerneldir/zeta/signing_tool/signing_tool"
+ln -s "$kerneldir/zeta/signing_tool/signing_tool.test"
 popd
 
 mkdir -p "WORK/$name"
